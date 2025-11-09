59779fe0e718d2fd96312bb745041137e1db8752
SewerKomp <seweryn.mlodawski@gmail.com>
2025-11-09 16:51:23 +0100

    Generate data for cards

--- 11_wizytowki_IV.py
+++ 11_wizytowki_IV.py
@@ -2,6 +2,24 @@ from faker import Faker  # import biblioteki Faker
 
 fake = Faker('pl_PL')
 
+def create_bizCards(contact_type, quantity):
+    biz_Cards = []
+    for _ in range(quantity):
+        imie = fake.first_name()
+        nazwisko = fake.last_name()
+        telefon = fake.phone_number()
+        email = fake.email()
+
+        if contact_type == 'base':
+            biz_Cards.append(BaseContact(imie, nazwisko, telefon, email))
+        elif contact_type == 'business':
+            stanowisko = fake.job()
+            firma = fake.company()
+            telefon_sluzbowy = fake.phone_number()
+
+            
+            
+
 class BaseContact:
     def __init__(self, imie, nazwisko, telefon, email):
         self.imie = imie
@@ -28,9 +46,9 @@ class BussinessContact(BaseContact):
     def contact(self):
         print(f"Wybieram numer: {self.telefon_sluzbowy} i dzwonię do: {self.imie} {self.nazwisko} z firmy {self.firma} na stanowisku {self.stanowisko}") 
 
-    @property 
-    def label_length(self):
-        return len(f"{self.imie} {self.nazwisko}")
+#    @property 
+#    def label_length(self):
+#        return len(f"{self.imie} {self.nazwisko}")
 
 
 
