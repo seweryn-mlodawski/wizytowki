76ad10fb99f3dcb9c93c90ae6b0e2852c95e767a
SewerKomp <seweryn.mlodawski@gmail.com>
2025-11-09 18:28:32 +0100

    Modyfing file, add faker

--- 11_wizytowki_IV.py
+++ 11_wizytowki_IV.py
@@ -2,6 +2,36 @@ from faker import Faker  # import biblioteki Faker
 
 fake = Faker('pl_PL')
 
+class BaseContact:
+    def __init__(self, imie, nazwisko, telefon, email):
+        self.imie = imie
+        self.nazwisko = nazwisko
+        self.telefon = telefon
+        self.email = email
+    
+    def contact(self):
+        print(f"Wybieram numer: {self.telefon} i dzwonię do: {self.imie} {self.nazwisko}")
+    def __str__(self):
+        return f"{self.imie} {self.nazwisko}, Tel: {self.telefon}, Email: {self.email}"
+        
+    @property
+    def label_length(self):
+        return len(f"{self.imie} {self.nazwisko}")
+
+class BusinessContact(BaseContact):
+    def __init__(self, imie, nazwisko, telefon, email, stanowisko, firma, telefon_sluzbowy):
+        super().__init__(imie, nazwisko, telefon, email)
+        self.stanowisko = stanowisko
+        self.firma = firma
+        self.telefon_sluzbowy = telefon_sluzbowy
+             
+    def contact(self):
+        print(f"Wybieram numer: {self.telefon_sluzbowy} i dzwonię do: {self.imie} {self.nazwisko} z firmy {self.firma} na stanowisku {self.stanowisko}")
+
+    def __str__(self):
+        return (f"{self.imie} {self.nazwisko}, Tel prywatny: {self.telefon}, Email: {self.email}, "
+                f"Stanowisko: {self.stanowisko}, Firma: {self.firma}, Tel służbowy: {self.telefon_sluzbowy}")
+
 def create_bizCards(contact_type, quantity):
     biz_Cards = []
     for _ in range(quantity):
@@ -16,52 +46,16 @@ def create_bizCards(contact_type, quantity):
             stanowisko = fake.job()
             firma = fake.company()
             telefon_sluzbowy = fake.phone_number()
-            
-            
+            biz_Cards.append(BusinessContact(imie, nazwisko, telefon, email, stanowisko, firma, telefon_sluzbowy))
+    return biz_Cards
 
-class BaseContact:
-    def __init__(self, imie, nazwisko, telefon, email):
-        self.imie = imie
-        self.nazwisko = nazwisko
-        self.telefon = telefon
-        self.email = email
-    
-    def contact(self):
-        print(f"Wybieram numer: {self.telefon} i dzwonię do: {self.imie} {self.nazwisko}")
-        
+base_contacts = create_bizCards('base', 5)
+business_contacts = create_bizCards('business', 5)
 
-    @property
-    def label_length(self):
-        return len(f"{self.imie} {self.nazwisko}")
+all_contacts = base_contacts + business_contacts
 
-
-class BussinessContact(BaseContact):
-    def __init__(self, imie, nazwisko, telefon, email, stanowisko, firma, telefon_sluzbowy):
-        super().__init__(imie, nazwisko, telefon, email)
-        self.stanowisko = stanowisko
-        self.firma = firma
-        self.telefon_sluzbowy = telefon_sluzbowy
-             
-    def contact(self):
-        print(f"Wybieram numer: {self.telefon_sluzbowy} i dzwonię do: {self.imie} {self.nazwisko} z firmy {self.firma} na stanowisku {self.stanowisko}") 
-
-#    @property 
-#    def label_length(self):
-#        return len(f"{self.imie} {self.nazwisko}")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+for contact in all_contacts:
+    print(contact)
+    contact.contact()
+    print(f"Długość etykiety: {contact.label_length}")
+    print()
