e11cd5fb928ba2690deb0c7f78d66e1796a7de07
Seweryn Młodawski <seweryn.mlodawski@gmail.com>
2025-12-28 20:16:10 +0100

    Add final ver cards task

--- testy.py
+++ testy.py
@@ -1,17 +1,28 @@
 1
 2
 3
-4
-5
-6
-7
-8
-9
-1
-11
-12
-#!/usr/bin/env python3
-134
-15   # Funkcja generująca wizytówki 
-16   def generate_business_cards(num_cards):
-17     import random
+4344
+
+ghgh     first_names = ["Jan", "Anna", "Piotr", "Katarzyna", "Michał"]
+ghgh     first_names = ["Jan", "Anna", "Piotr", "Katarzyna", "Michał"]
+5556666
+ghgh     first_names = ["Jan", "Anna", "Piotr", "Katarzyna", "Michał"]
+ghgh     first_names = ["Jan", "Anna", "Piotr", "Katarzyna", "Michał"]
+
+ghgh     first_names = ["Jan", "Anna", "Piotr", "Katarzyna", "Michał"]
+
+ghgh     first_names = ["Jan", "Anna", "Piotr", "Katarzyna", "Michał"]
+
+ghgh     first_names = ["Jan", "Anna", "Piotr", "Katarzyna", "Michał"]
+
+ghgh     first_names = ["Jan", "Anna", "Piotr", "Katarzyna", "Michał"]/usr/bin/env python3
+
+ghgh     first_names = ["Jan", "Anna", "Piotr", "Katarzyna", "Michał"]4
+
+ghgh     first_names = ["Jan", "Anna", "Piotr", "Katarzyna", "Michał"] 
+  # Funkcja generująca wizytówki 
+ghgh     first_names = ["Jan", "Anna", "Piotr", "Katarzyna", "Michał"] 
+  def generate_business_cards(num_cards):
+ghgh     first_names = ["Jan", "Anna", "Piotr", "Katarzyna", "Michał"] 
+    import random
+ghgh     first_names = ["Jan", "Anna", "Piotr", "Katarzyna", "Michał"]
